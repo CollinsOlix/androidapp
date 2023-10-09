4e0323e2fabf4ab63eb86ac392c48017d12ae013
CollinsOlix <113027794+CollinsOlix@users.noreply.github.com>
2023-10-09 22:33:31 +0300

    Made changes to app name and re bundled

--- ios/CurrencyConverterMiniTests/CurrencyConverterMiniTests.m
+++ ios/CurrencyConverterMiniTests/CurrencyConverterMiniTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface androidappTests : XCTestCase
+@interface CurrencyConverterMiniTests : XCTestCase
 
 @end
 
-@implementation androidappTests
+@implementation CurrencyConverterMiniTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
